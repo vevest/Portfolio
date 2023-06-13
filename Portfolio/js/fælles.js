560ae4acb2be21da09d2c75370c3993150057761
vevest <132901408+vevest@users.noreply.github.com>
2023-06-13 21:11:09 +0200

    Add files via upload

---
+++ Portfolio/js/fælles.js
@@ -0,0 +1,17 @@
+
+// -----------------hamburger menu-----------------------
+
+window.onload = function (){
+
+    const menu_btn = document.querySelector('.hamburger');
+    const mobil_menu = document.querySelector('.mobil-nav');
+  
+    menu_btn.addEventListener('click', function () {
+  
+    menu_btn.classList.toggle('is-active');
+    mobil_menu.classList.toggle('is-active');
+  
+  
+  });
+  
+  }
